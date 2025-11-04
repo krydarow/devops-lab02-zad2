2bc224a780d1475b5e5d6cb28bcb6a81384c2a58
Kamil <krydarow@student.wszib.edu.pl>
2025-11-04 20:05:45 +0100

    java code and test

---
+++ devops-lab2-zad2/src/test/java/org/example/ExampleTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExampleTest {
+
+    @Test
+    void dummyTest() {
+        assertTrue(true);
+    }
+}
